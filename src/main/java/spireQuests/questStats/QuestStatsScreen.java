4ca827cd3e41beda2c9c9ed630bab78ef8ddbff3
Anthony Mercado <mercado.anthony.e@gmail.com>
2025-11-23 22:49:39 -0500

    some more stuff

--- src/main/java/spireQuests/questStats/QuestStatsScreen.java
+++ src/main/java/spireQuests/questStats/QuestStatsScreen.java
@@ -31,17 +31,42 @@ public class QuestStatsScreen implements DropdownMenuListener {
         public static MainMenuScreen.CurScreen QUEST_STATS_SCREEN;
     }
 
-    private static final Logger logger = LogManager.getLogger(MainMenuScreen.class.getName());
-    private static final float DROPDOWN_X = 20.0F * Settings.scale;
-    private static final float DROPDOWN_Y = Settings.HEIGHT/2f + 340.0F * Settings.scale;
+    private static final Logger logger = LogManager.getLogger(QuestStatsScreen.class.getName());
+
+    private static final float LEFT_X_ANCHOR = 490.0F * Settings.xScale;
+
+    private static final float DROPDOWN_Y = 780.0F * Settings.yScale;
+
+    private static final float QUEST_NAME_Y = 715.0F * Settings.yScale;
+    private static final float QUEST_AUTHOR_Y = 675.0F * Settings.yScale;
+    private static final float QUEST_DESCRIPTION_Y = 650.0F * Settings.yScale;
+    private static final float QUEST_DESCRIPTION_LENGTH = 700.0F * Settings.xScale;
+
+    private static final float QUEST_SEEN_Y = 425.0F * Settings.yScale;
+    private static final float QUEST_TAKEN_Y = 375.0F * Settings.yScale;
+    private static final float QUEST_COMPLETE_Y = 325.0F * Settings.yScale;
+    private static final float QUEST_FAILED_Y = 275.0F * Settings.yScale;
+
     private MenuCancelButton cancelButton = new MenuCancelButton();
     private DropdownMenu questDropdown;
-    private Map<String, String> allQuests;
-    private String selectedQuestID;
+
+    private Collection<AbstractQuest> allQuests;
+    private Map<String, AbstractQuest> allQuestsMap;
+    private ArrayList<String> allQuestList;
+    private AbstractQuest selectedQuest;
+    private QuestStats selectedQuestStats;
+    private Map<String, String> nameIDMap;
+
+    private int timesSeen = 0;
+    private int timesTaken = 0;
+    private int timesCompleted = 0;
+    private int timesFailed = 0;
     
     public QuestStatsScreen() {
-        allQuests = QuestManager.getAllQuests().stream().collect(Collectors.toMap(q -> q.name, q -> q.id));
-        ArrayList<String> allQuestList = new ArrayList<>(allQuests.keySet());
+        allQuests = QuestManager.getAllQuests();
+        allQuestsMap = allQuests.stream().collect(Collectors.toMap(q -> q.id, q -> q));
+        nameIDMap = allQuests.stream().collect(Collectors.toMap(q -> q.name, q -> q.id));
+        allQuestList = new ArrayList<>(allQuestsMap.values().stream().map(q -> q.name).collect(Collectors.toList()));
         allQuestList.sort(null);
         allQuestList.add(0, "TEXT[ALL_QUESTS]");
         questDropdown = new DropdownMenu(this, allQuestList, FontHelper.tipBodyFont, Settings.CREAM_COLOR);
@@ -75,17 +100,71 @@ public class QuestStatsScreen implements DropdownMenuListener {
     }
 
     public void render(SpriteBatch sb) {
-        questDropdown.render(sb, DROPDOWN_X, DROPDOWN_Y);
+        renderStats(sb);
+        renderTrophy(sb);
+        questDropdown.render(sb, LEFT_X_ANCHOR, DROPDOWN_Y);
         cancelButton.render(sb);
+    }
+
+    private void renderStats(SpriteBatch sb) {
+        // Name
+        if (selectedQuest == null) {
+            FontHelper.renderFont(sb, FontHelper.tipHeaderFont, "TEXT[ALL_QUESTS]", LEFT_X_ANCHOR, QUEST_NAME_Y, Settings.CREAM_COLOR);
+        } else {
+            FontHelper.renderFont(sb, FontHelper.tipHeaderFont, selectedQuest.name, LEFT_X_ANCHOR, QUEST_NAME_Y, Settings.CREAM_COLOR);
+        }
+
+        // Quest Info
+        if (selectedQuest == null) {
+            // General Quest Info
+        } else {
+            // Author
+            FontHelper.renderFont(sb, FontHelper.tipBodyFont, selectedQuest.author, LEFT_X_ANCHOR, QUEST_AUTHOR_Y, Settings.CREAM_COLOR);
+            // Description
+            FontHelper.renderSmartText(
+                sb, FontHelper.tipBodyFont, 
+                selectedQuest.description, 
+                LEFT_X_ANCHOR, QUEST_DESCRIPTION_Y, QUEST_DESCRIPTION_LENGTH,
+                FontHelper.tipBodyFont.getLineHeight(),
+                Settings.CREAM_COLOR
+            );
+            // Stats
+            FontHelper.renderFont(sb, FontHelper.tipBodyFont, 
+                String.format("%s: %d", "TEXT[SEEN]", timesSeen), LEFT_X_ANCHOR, QUEST_SEEN_Y, Settings.CREAM_COLOR);
+            FontHelper.renderFont(sb, FontHelper.tipBodyFont, 
+                String.format("%s: %d/%d (%.2f%%)", "TEXT[TAKEN]", timesTaken, timesSeen, ((timesTaken * 100.0f)/timesSeen)), 
+                LEFT_X_ANCHOR, QUEST_TAKEN_Y, Settings.CREAM_COLOR);
+            FontHelper.renderFont(sb, FontHelper.tipBodyFont, 
+                String.format("%s: %d/%d (%.2f%%)", "TEXT[COMPLETE]", timesCompleted, timesTaken, ((timesCompleted * 100.0f)/timesTaken)), 
+                LEFT_X_ANCHOR, QUEST_COMPLETE_Y, Settings.CREAM_COLOR);
+            FontHelper.renderFont(sb, FontHelper.tipBodyFont, 
+                String.format("%s: %d/%d (%.2f%%)", "TEXT[FAILED]", timesFailed, timesTaken, ((timesFailed * 100.0f)/timesTaken)), 
+                LEFT_X_ANCHOR, QUEST_FAILED_Y, Settings.CREAM_COLOR);
+        }
+    }
+
+    private void renderTrophy(SpriteBatch sb) {
+
     }
 
     @Override
     public void changedSelectionTo(DropdownMenu dropdownMenu, int i, String s) {
+        logger.warn("Changed dropdown detected");
         if (i == 0) {
-            selectedQuestID = null;
+            selectedQuestStats = QuestStats.getAllStats();
+            selectedQuest = null;
         } else {
-            selectedQuestID = allQuests.get(s);
+            String qid = nameIDMap.get(s);
+            selectedQuestStats = new QuestStats(qid);
+            selectedQuest = allQuestsMap.get(qid);
         }
-        // refreshPage();
+        refreshData();
+    }
+
+    private void refreshData() {
+        timesSeen = selectedQuestStats.timesSeen;
+        timesTaken = selectedQuestStats.timesTaken;
+        timesCompleted = selectedQuestStats.timesComplete;
+        timesFailed = selectedQuestStats.timesFailed;
     }
 }
