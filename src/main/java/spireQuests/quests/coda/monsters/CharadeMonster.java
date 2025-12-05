b71920cf069f1be31a4a2670f917cdde9c184657
Anthony Mercado <mercado.anthony.e@gmail.com>
2025-12-05 02:31:45 -0500

    hodl

--- src/main/java/spireQuests/quests/coda/monsters/CharadeMonster.java
+++ src/main/java/spireQuests/quests/coda/monsters/CharadeMonster.java
@@ -11,11 +11,10 @@ import java.util.Random;
 import com.esotericsoftware.spine.AnimationState;
 import com.megacrit.cardcrawl.actions.AbstractGameAction.AttackEffect;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.ChangeStateAction;
 import com.megacrit.cardcrawl.actions.common.DamageAction;
 import com.megacrit.cardcrawl.actions.common.GainBlockAction;
-import com.megacrit.cardcrawl.actions.common.HealAction;
 import com.megacrit.cardcrawl.actions.common.MakeTempCardInDiscardAction;
-import com.megacrit.cardcrawl.actions.common.RemoveSpecificPowerAction;
 import com.megacrit.cardcrawl.actions.common.RollMoveAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.DamageInfo;
@@ -24,15 +23,12 @@ import com.megacrit.cardcrawl.cards.status.Burn;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.MonsterStrings;
-import com.megacrit.cardcrawl.powers.AbstractPower;
-import com.megacrit.cardcrawl.powers.MetallicizePower;
+import com.megacrit.cardcrawl.powers.ArtifactPower;
 import com.megacrit.cardcrawl.powers.PlatedArmorPower;
 import com.megacrit.cardcrawl.powers.StrengthPower;
-import com.megacrit.cardcrawl.powers.VulnerablePower;
 import com.megacrit.cardcrawl.powers.WeakPower;
 
 import spireQuests.abstracts.AbstractSQMonster;
-import spireQuests.quests.coda.powers.FakeVigorPower;
 import spireQuests.util.Wiz;
 public class CharadeMonster extends AbstractSQMonster {
 
@@ -46,7 +42,7 @@ public class CharadeMonster extends AbstractSQMonster {
     private static final float HB_W = 420.0F;
     private static final float HB_H = 250.0F; //534
 
-    private static final Byte R_A = 0, G_A = 1, B_A = 2, P_A = 3, R_D = 4, G_D = 5, B_D = 6, P_D = 7, R_S = 8, G_S = 9, B_S = 10, P_S = 11;
+    private static final Byte IRONCLAD = 0, SILENT = 2, DEFECT = 3, WATCHER = 4, WAKEUP = 5;
 
     private enum OrbColor {
         RED,
@@ -54,25 +50,17 @@ public class CharadeMonster extends AbstractSQMonster {
         BLUE,
         PURPLE
     }
-    private enum MoveType {
-        OFFENSE,
-        DEFENSE,
-        SPECIAL
-    }
 
     private OrbColor currColor;
-    private MoveType currMoveType;
     private ArrayList<OrbColor> validColors = new ArrayList<>();
-    private ArrayList<MoveType> validMoves = new ArrayList<>();
-    private int greenAttackCount;
-    private int redSpecialDebuff;
+    private int greenAttackAmount;
+    private int redDebuffAmount;
     private int purpleDefendBuff;
-    private int blueBlockCount;
-    private int greenSpecialDebuff;
-    private int redAttackDebuff;
+    private int greenDebuffAmount;
     private int turnsTaken;
-    private int greenDefendDebuff;
-    private int blueSpecialAmount;
+    private int blueBuffAmount;
+    private int purpleAttackBuff;
+    private boolean isAwake;
     
     public CharadeMonster() {
         this(0, 200);
@@ -84,52 +72,37 @@ public class CharadeMonster extends AbstractSQMonster {
  
         type = EnemyType.ELITE;
 
+        this.isAwake = false;
+
         validColors = new ArrayList(Arrays.asList(OrbColor.values()));
         Collections.shuffle(validColors, new Random(AbstractDungeon.aiRng.randomLong()));
-        validMoves = new ArrayList();
-            validMoves.add(MoveType.OFFENSE);
-            validMoves.add(MoveType.DEFENSE);
-            validMoves.add(MoveType.OFFENSE);
-            validMoves.add(MoveType.SPECIAL);
-            validMoves.add(MoveType.OFFENSE);
-            validMoves.add(MoveType.DEFENSE);
+
         this.turnsTaken = 0;
-        this.redAttackDebuff = 3;
-        this.redSpecialDebuff = 3;
-        this.greenAttackCount = 4;
-        this.greenDefendDebuff = 2;
-        this.blueBlockCount = 0;
-        this.purpleDefendBuff = 10;
-        this.greenSpecialDebuff = 5;
-        this.blueSpecialAmount = calcAscensionTankiness(75);
+        this.redDebuffAmount = 3;
+        this.greenAttackAmount = 4;
+        this.greenDebuffAmount = 1;
+        this.blueBuffAmount = 2;
+        this.purpleDefendBuff = 8;
+        this.purpleAttackBuff = 1;
 
         if (AbstractDungeon.ascensionLevel >= 3) {
-            this.greenAttackCount = 5;
-            
+            this.greenAttackAmount = 5;
         }
         if (AbstractDungeon.ascensionLevel >= 8) {
-            this.blueBlockCount = 1;
+            this.purpleDefendBuff = 12;
         }
         if (AbstractDungeon.ascensionLevel >= 18) {
-            this.redAttackDebuff = 6;
-            this.redSpecialDebuff = 5;
-            this.greenDefendDebuff = 4;
-            this.greenSpecialDebuff = 8;
+            this.redDebuffAmount = 5;
+            this.greenDebuffAmount = 2;
+            this.blueBuffAmount = 5;
+            this.purpleAttackBuff = 2;
         }
-        
 
-        addMove(R_A, Intent.ATTACK, calcAscensionDamage(20));
-        addMove(R_D, Intent.DEFEND);
-        addMove(R_S, Intent.DEBUFF);
-        addMove(G_A, Intent.ATTACK, calcAscensionDamage(6), greenAttackCount, true);
-        addMove(G_D, Intent.DEFEND_BUFF);
-        addMove(G_S, Intent.DEBUFF);
-        addMove(B_A, Intent.ATTACK, calcAscensionDamage(40));
-        addMove(B_D, Intent.DEFEND);
-        addMove(B_S, Intent.UNKNOWN);
-        addMove(P_A, Intent.ATTACK_DEFEND, calcAscensionDamage(20));
-        addMove(P_D, Intent.DEFEND);
-        addMove(P_S, Intent.BUFF);
+        addMove(IRONCLAD, Intent.DEFEND_DEBUFF);
+        addMove(SILENT, Intent.ATTACK_DEBUFF, calcAscensionDamage(5), greenAttackAmount, true);
+        addMove(DEFECT, Intent.ATTACK_BUFF, calcAscensionDamage(40));
+        addMove(WATCHER, Intent.DEFEND_BUFF);
+        addMove(WAKEUP, Intent.UNKNOWN);
 
         setHp(calcAscensionTankiness(HP_MIN), calcAscensionTankiness(HP_MAX));
         
@@ -157,72 +130,57 @@ public class CharadeMonster extends AbstractSQMonster {
     }
 
     @Override
+    public void usePreBattleAction() {
+        if (!this.isAwake) {
+            addToBot(new GainBlockAction(this, 100));
+        }
+    }
+
+    @Override
     protected void getMove(int i) {
-        getRandomMove();
+        if (this.firstMove) {
+            setMoveShortcut(WAKEUP);
+            this.firstMove = false;
+            return;
+        }
+        currColor = validColors.get(this.turnsTaken % validColors.size());
+        changeColor(currColor);
         switch (currColor) {
             case RED:
-                switch (currMoveType) {
-                    case DEFENSE:
-                        setMoveShortcut(R_D);
-                        break;
-                    case OFFENSE:
-                        setMoveShortcut(R_A);
-                        break;
-                    case SPECIAL:
-                        setMoveShortcut(R_S);
-                        break;
-                }
+                setMoveShortcut(IRONCLAD);
                 break;
             case GREEN:
-                switch (currMoveType) {
-                    case DEFENSE:
-                        setMoveShortcut(G_D);
-                        break;
-                    case OFFENSE:
-                        setMoveShortcut(G_A);
-                        break;
-                    case SPECIAL:
-                        setMoveShortcut(G_S);
-                        break;
-                }
+                setMoveShortcut(SILENT);
                 break;
             case BLUE:
-                switch (currMoveType) {
-                    case DEFENSE:
-                        setMoveShortcut(B_D);
-                        break;
-                    case OFFENSE:
-                        setMoveShortcut(B_A);
-                        break;
-                    case SPECIAL:
-                        setMoveShortcut(B_S);
-                        break;
-                }
+                setMoveShortcut(DEFECT);
                 break;
             case PURPLE:
-                switch (currMoveType) {
-                    case DEFENSE:
-                        setMoveShortcut(P_D);
-                        break;
-                    case OFFENSE:
-                        setMoveShortcut(P_A);
-                        break;
-                    case SPECIAL:
-                        setMoveShortcut(P_S);
-                        break;
-                }
+                setMoveShortcut(WATCHER);
                 break;
         }
     }
-    private void getRandomMove() {
-        currColor = validColors.get(this.turnsTaken % validColors.size());
-        changeColor(currColor);
-        currMoveType = validMoves.get(this.turnsTaken % validMoves.size());
+
+    @Override
+    public void changeState(String stateName) {
+        switch (stateName) {
+            case "AWAKE":
+                this.isAwake = true;
+                state.setAnimation(0, "wake", false);
+                state.addAnimation(0, "idle", true, 0.0F);
+                break;
+            case "ATTACK":
+                state.setAnimation(0, "attack", false);
+                state.addAnimation(0, "idle", true, 0.0F);
+                break;
+        }
     }
 
     @Override
     public void damage(DamageInfo info) {
-        if (info.owner != null && info.type != DamageType.THORNS && info.output - currentBlock > 0) {
+        if (!this.isAwake) {
+            addToBot(new ChangeStateAction(this, "AWAKE"));
+        } else if (info.owner != null && info.type != DamageType.THORNS && info.output - currentBlock > 0) {
             AnimationState.TrackEntry e = state.setAnimation(0, "hit", false);
             state.addAnimation(0, "idle", true, 0.0F);
             e.setTimeScale(1.0f);
@@ -255,80 +213,40 @@ public class CharadeMonster extends AbstractSQMonster {
 
     @Override
     public void takeTurn() {
-        int vigorAmount = 0;
-        if (this.hasPower(FakeVigorPower.ID)) {
-            vigorAmount = this.getPower(FakeVigorPower.ID).amount;
-        }
-        DamageInfo info = new DamageInfo(this, moves.get(nextMove).baseDamage + vigorAmount, DamageInfo.DamageType.NORMAL);
+        DamageInfo info = new DamageInfo(this, moves.get(nextMove).baseDamage, DamageInfo.DamageType.NORMAL);
         info.applyPowers(this, AbstractDungeon.player);
         this.turnsTaken++;
 
         switch (nextMove) {
-            case 0: // Red Attack: Uppercut
-                addToBot(new DamageAction(Wiz.p(), info, AttackEffect.SMASH));
-                addToBot(new ApplyPowerAction(Wiz.p(), this, new VulnerablePower(Wiz.p(), this.redAttackDebuff, true)));
-                addToBot(new ApplyPowerAction(Wiz.p(), this, new WeakPower(Wiz.p(), this.redAttackDebuff, true)));
-                clearVigor();
-                break;
-            case 1: // Green Attack: Blade Dance
-                for (int i = 0; i < this.greenAttackCount; i++ ) {
-                    addToBot(new DamageAction(Wiz.p(), info, AttackEffect.SLASH_HORIZONTAL));
-                }
-                clearVigor();
-                break;
-            case 2: // Blue Attack: Hyperbeam
-                addToBot(new DamageAction(Wiz.p(), info));
-                clearVigor();
-                break;
-            case 3: // Purple Attack: Wallop
-                addToBot(new DamageAction(Wiz.p(), info, AttackEffect.BLUNT_HEAVY));
-                addToBot(new GainBlockAction(this, this, Wiz.p().lastDamageTaken));
-                clearVigor();
-                break;
-            case 4: // Red Block: Impervious
+            case 0: // IRONCLAD
                 addToBot(new GainBlockAction(this, 30));
-                break;
-            case 5: // Green Block: Leg Sweep
-                addToBot(new GainBlockAction(this, 20));
-                addToBot(new ApplyPowerAction(Wiz.p(), this, new WeakPower(Wiz.p(), this.greenDefendDebuff, true)));
-                break;
-            case 6: // Blue Block: Genetic Algorithm
-                addToBot(new GainBlockAction(this, 10 * (this.blueBlockCount + 1)));
-                this.blueBlockCount += 1;
-                break;
-            case 7: // Purple Block: Wish (Plated Armor)
-                AbstractPower pta;
-                if (AbstractDungeon.ascensionLevel >= 8) {
-                    pta = new MetallicizePower(this, purpleDefendBuff);
-                } else {
-                    pta = new PlatedArmorPower(this, purpleDefendBuff);
-                }
-                addToBot(new ApplyPowerAction(this, this, pta));
-                break;
-            case 8: // Red Special: Immolate
                 AbstractCard burn = new Burn();
                 burn.upgrade();
-                addToBot(new MakeTempCardInDiscardAction(burn, redSpecialDebuff));
+                addToBot(new MakeTempCardInDiscardAction(burn, redDebuffAmount));
                 break;
-            case 9: // Green Special: Malaise
-                addToBot(new ApplyPowerAction(Wiz.p(), this, new StrengthPower(Wiz.p(), -1 * this.greenSpecialDebuff)));
-                addToBot(new ApplyPowerAction(Wiz.p(), this, new WeakPower(Wiz.p(), this.greenSpecialDebuff, true)));
+            case 1: // SILENT
+                for (int i = 0; i < this.greenAttackAmount; i++ ) {
+                    addToBot(new DamageAction(Wiz.p(), info, AttackEffect.SLASH_HORIZONTAL));
+                }
+
+                addToBot(new ApplyPowerAction(Wiz.p(), this, new StrengthPower(Wiz.p(), -1 * this.greenDebuffAmount)));
+                addToBot(new ApplyPowerAction(Wiz.p(), this, new WeakPower(Wiz.p(), this.greenDebuffAmount, true)));
                 break;
-            case 10: // Blue Special: Self Repair
-                addToBot(new HealAction(this, this, this.blueSpecialAmount));
+            case 2: // DEFECT
+                addToBot(new DamageAction(Wiz.p(), info));
+                addToBot(new ApplyPowerAction(Wiz.p(), this, new ArtifactPower(this, blueBuffAmount)));
                 break;
-            case 11: // Purple Special: Akkebeko
-                addToBot(new ApplyPowerAction(this, this, new FakeVigorPower(this, Math.max(1, this.turnsTaken))));
+            case 3: // WATCHER
+                addToBot(new ApplyPowerAction(this, this, new PlatedArmorPower(this, purpleDefendBuff)));
+                addToBot(new ApplyPowerAction(this, this, new StrengthPower(Wiz.p(), this.purpleAttackBuff)));
                 break;
-        }
+            case 4: // WAKE UP
+                if (!this.isAwake) {
+                    addToBot(new ChangeStateAction(this, "AWAKE"));
+                }
+            }
 
         addToBot(new RollMoveAction(this));
     }
 
-    private void clearVigor() {
-        if (this.hasPower(FakeVigorPower.ID)){
-            addToBot(new RemoveSpecificPowerAction(this, this, this.getPower(FakeVigorPower.ID)));
-        }
-    }
-    
 }
