b1704b39ab5a09155cda635b95caac38f57e2ae1
Anthony Mercado <mercado.anthony.e@gmail.com>
2025-12-15 20:20:54 -0500

    Update trackers

--- src/main/java/spireQuests/quests/coda/KeyCollector.java
+++ src/main/java/spireQuests/quests/coda/KeyCollector.java
@@ -12,12 +12,45 @@ public class KeyCollector extends AbstractQuest {
     public KeyCollector() {
         super(QuestType.LONG, QuestDifficulty.CHALLENGE);
 
-        new PassiveTracker<Boolean>(() -> Settings.hasRubyKey && Settings.hasEmeraldKey && Settings.hasSapphireKey, true)
+        new PassiveTracker<Boolean>(() -> Settings.hasRubyKey, true)
             {
                 @Override
                 public String progressString() {
                     return "";
                 }
+
+                @Override
+                public boolean hidden() {
+                    return Settings.hasRubyKey;
+                };
+            }
+            .setFailureTrigger(QuestTriggers.ACT_CHANGE)
+            .add(this);
+        new PassiveTracker<Boolean>(() -> Settings.hasEmeraldKey, true)
+            {
+                @Override
+                public String progressString() {
+                    return "";
+                }
+
+                @Override
+                public boolean hidden() {
+                    return Settings.hasEmeraldKey;
+                };
+            }
+            .setFailureTrigger(QuestTriggers.ACT_CHANGE)
+            .add(this);
+        new PassiveTracker<Boolean>(() -> Settings.hasSapphireKey, true)
+            {
+                @Override
+                public String progressString() {
+                    return "";
+                }
+
+                @Override
+                public boolean hidden() {
+                    return Settings.hasSapphireKey;
+                };
             }
             .setFailureTrigger(QuestTriggers.ACT_CHANGE)
             .add(this);
