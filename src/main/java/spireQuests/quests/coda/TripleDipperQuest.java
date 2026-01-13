82fc71394d29fd1d2d2e3e1b01d151fd5fd4312f
Anthony Mercado <mercado.anthony.e@gmail.com>
2026-01-12 21:03:48 -0500

    add triple dipper quest

---
+++ src/main/java/spireQuests/quests/coda/TripleDipperQuest.java
@@ -0,0 +1,36 @@
+package spireQuests.quests.coda;
+
+import com.megacrit.cardcrawl.cards.AbstractCard.CardType;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.relics.OrangePellets;
+
+import spireQuests.patches.QuestTriggers;
+import spireQuests.quests.AbstractQuest;
+import spireQuests.quests.QuestReward;
+
+public class TripleDipperQuest extends AbstractQuest {
+
+    public TripleDipperQuest() {
+        super(QuestType.LONG, QuestDifficulty.NORMAL);
+
+        new TriggerTracker<>(QuestTriggers.ADD_CARD, 3)
+            .triggerCondition((c) -> c.type == CardType.ATTACK)
+            .add(this);
+
+        new TriggerTracker<>(QuestTriggers.ADD_CARD, 3)
+            .triggerCondition((c) -> c.type == CardType.SKILL)
+            .add(this);
+
+        new TriggerTracker<>(QuestTriggers.ADD_CARD, 3)
+            .triggerCondition((c) -> c.type == CardType.POWER)
+            .add(this);
+
+        addReward(new QuestReward.RelicReward(new OrangePellets()));
+    }
+
+    @Override
+    public boolean canSpawn() {
+        return AbstractDungeon.actNum == 2;
+    }
+
+}
